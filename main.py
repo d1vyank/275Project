9b5c185a68fc3e0df55d0c8c9da09047f75b9c88
d1vyank <divyanktk@gmail.com>
2014-04-06 15:59:27 -0600

    bug fix

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import sys
 DEBUG = True
 
 if __name__ == "__main__":
-	try:
-		get_history()
-		FILE_NAME = 'out.csv'
-	except OperationalError:
-		print("Chrome is running.. Using demo history file")
-		FILE_NAME = 'history.csv'	
+    try:
+        get_history()
+        FILE_NAME = 'out.csv'
+    except OperationalError:
+        print("Chrome is running.. Using demo history file")
+        FILE_NAME = 'history.csv'	
 	
     g, md = load_graph(FILE_NAME)
     g = find_edges(g, md)
